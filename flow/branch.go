881b1dc01b4081d6540e6e44b7345a6ec72ba285
chenzhihui <zhihui_chen@foxmail.com>
2025-10-09 16:37:24 +0800

    add branch node

---
+++ flow/branch.go
@@ -0,0 +1,58 @@
+package flow
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-kratos/blades"
+)
+
+// BranchSelector is a function that selects a branch name based on the context.
+type BranchSelector func(context.Context) (string, error)
+
+// Branch represents a branching structure of Runnable runners that process input based on a selector function.
+type Branch[I, O, Option any] struct {
+	selector BranchSelector
+	runners  map[string]blades.Runner[I, O, Option]
+}
+
+// NewBranch creates a new Branch with the given selector and runners.
+func NewBranch[I, O, Option any](selector BranchSelector, runners map[string]blades.Runner[I, O, Option]) *Branch[I, O, Option] {
+	return &Branch[I, O, Option]{
+		selector: selector,
+		runners:  runners,
+	}
+}
+
+// Run executes the selected runner based on the selector function.
+func (c *Branch[I, O, Option]) Run(ctx context.Context, input I, opts ...Option) (O, error) {
+	var (
+		err    error
+		output O
+	)
+	name, err := c.selector(ctx)
+	if err != nil {
+		return output, err
+	}
+	runner, ok := c.runners[name]
+	if !ok {
+		return output, fmt.Errorf("Branch: runner not found: %s", name)
+	}
+	return runner.Run(ctx, input, opts...)
+}
+
+// RunStream executes the selected runner based on the selector function and streams its output.
+func (c *Branch[I, O, Option]) RunStream(ctx context.Context, input I, opts ...Option) (blades.Streamer[O], error) {
+	pipe := blades.NewStreamPipe[O]()
+	pipe.Go(func() error {
+		for _, runner := range c.runners {
+			output, err := runner.Run(ctx, input, opts...)
+			if err != nil {
+				return err
+			}
+			pipe.Send(output)
+		}
+		return nil
+	})
+	return pipe, nil
+}
